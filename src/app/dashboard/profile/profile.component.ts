197f560f3b9e27acd46bcf96980f7677d846d694
Ellen <eegreen.95@gmail.com>
2020-06-23 16:51:19 -0500

    moved date to right col

--- src/app/dashboard/profile/profile.component.ts
+++ src/app/dashboard/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from 'src/app/shared/firebase.service';
+import { FormControl } from '@angular/forms';
+import { AuthenticationService } from 'src/app/shared/authentication.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,14 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: FirebaseService, private auth: AuthenticationService) { }
 
-  ngOnInit(): void {
-  }
+  savedFish=[];
+  savedBugs=[];
+  userInfo=[];
+  fishWidth: number;
+  bugWidth: number;
 
   displayName: string;
-  addName(name: string){
-    this.displayName = name;
+  nativeFlower: any;
+  nativeFruit: any;
+  islandName: any;
+  flowers = ['Lilies', 'Roses', 'Hyacinths', 'Pansies', 'Mums', 'Cosmos', 'Tulips', 'Windflowers'];
+  fruit = ['Apples', 'Oranges', 'Peaches', 'Pears', 'Cherries'];
+  new: boolean;
+  submitted = false;
+
+  ngOnInit() {
+    this.db.fetchFish().subscribe(fish =>{
+      this.savedFish=fish;
+      this.fishWidth = (this.savedFish.length/80)*100;
+    })
+    this.db.fetchBugs().subscribe(bugs=> {
+      this.savedBugs=bugs;
+      this.bugWidth = (this.savedBugs.length/80)*100;
+    })
+    this.db.fetchUserInfo().subscribe(data=> {
+      this.userInfo=data;
+      if (this.userInfo.length < 1){
+        this.new = true;
+        } else {
+          this.new = false;
+        }
+      console.log(this.userInfo)
+    })
   }
 
+  onSubmit() { this.submitted = true; }
+  update(profileForm){
+  this.db.addUserInfo(profileForm.value)
+  console.log(profileForm.value)
+  window.location.reload();
+    
+  }
 }
+
+
+
+
+ 
