5309deb862d11b145252833b21aacd9569bab7d6
Ellen <eegreen.95@gmail.com>
2020-06-27 01:12:12 -0500

    C:/Program Files/Git/

--- src/app/shared/authentication.service.ts
+++ src/app/shared/authentication.service.ts
@@ -2,32 +2,35 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { User, auth } from 'firebase';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-  //variable to store user data
+
   user: User;
+  id: any;
+
   constructor(public afAuth: AngularFireAuth, public router: Router) {
-    //subscribe to auth state
     this.afAuth.authState.subscribe(user => {
-      //if user is logged in, add their data to local storage
       if (user){
         this.user = user;
         localStorage.setItem(`user`, JSON.stringify(this.user));
-        //otherwise, store null user
       } else { 
         localStorage.setItem('user', null);
-      }
+      } 
     })
+   }
+
+   currentUser:any;
+   getUser(){
+    return this.currentUser = JSON.parse(localStorage.getItem('user'));
    }
 
    //login with email & password
    async login(email, password){
     try {
-       const result = await this.afAuth.signInWithEmailAndPassword(email, password);
+      await this.afAuth.signInWithEmailAndPassword(email, password);
        this.router.navigate(['home']);
      }
      catch (error) {
@@ -47,11 +50,6 @@ export class AuthenticationService {
     })
   }
 
-   //send email to reset password
-   async resetPasswordEmail(passwordResetEmail: string){
-    return await this.afAuth.sendPasswordResetEmail(passwordResetEmail)
-   }
-
    //logout of account
    async logout(){
      await this.afAuth.signOut();
@@ -59,8 +57,7 @@ export class AuthenticationService {
      this.router.navigate(['login'])
    }
 
-   //check if user is logged in
-   get isLoggedIn(): boolean {
+   get isLoggedIn(){
      const user = JSON.parse(localStorage.getItem('user'));
      return user !== null;
    }
@@ -68,6 +65,8 @@ export class AuthenticationService {
    //login with google account
    async loginWithGoogle(){
      await this.afAuth.signInWithPopup(new auth.GoogleAuthProvider())
-     this.router.navigate(['profile']);
+     this.router.navigate(['home']);
    }
+
+
 }
---
+++ src/app/shared/firebase.service.ts
@@ -0,0 +1,83 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { AuthenticationService } from './authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+user: any;
+id: any;
+
+  constructor(private http: HttpClient, private auth: AuthenticationService) { 
+    this.user = this.auth.getUser()
+    this.id = this.user['uid'];
+  }
+
+  addBug(selectedCritter){
+    this.http
+    .post(`https://animal-crossing-92e14.firebaseio.com/users/${this.id}/bugs.json`, selectedCritter)
+    .subscribe(responseData=>{
+    })
+  }
+
+  addFish(selectedCritter){
+    this.http
+    .post(`https://animal-crossing-92e14.firebaseio.com/users/${this.id}/fish.json`, selectedCritter)
+    .subscribe(responseData=>{
+    });
+  }
+
+  fetchBugs(){
+    return this.http
+    .get(`https://animal-crossing-92e14.firebaseio.com/users/${this.id}/bugs.json`)
+    .pipe(map(responseData => {
+      const loadedBugs =[];
+      for (const key in responseData) {
+        if (responseData.hasOwnProperty(key)){
+          loadedBugs.push({ ...responseData[key], newID: key})
+        }
+      }
+      return loadedBugs;
+    })
+    )
+  }
+
+  fetchFish(){
+    return this.http
+    .get(`https://animal-crossing-92e14.firebaseio.com/users/${this.id}/fish.json`)
+    .pipe(map(responseData => {
+      const loadedFish =[];
+      for (const key in responseData) {
+        if (responseData.hasOwnProperty(key)){
+          loadedFish.push({ ...responseData[key], newID: key})
+        }
+      }
+      return loadedFish;
+    })
+    )
+  }
+
+  addUserInfo(profileData: {name: string, islandName: string, nativeFruit: any, nativeFlower: any}){
+    this.http
+    .post(`https://animal-crossing-92e14.firebaseio.com/users/${this.id}/profile.json`, profileData)
+    .subscribe(responseData=>{
+    })
+  }
+
+  fetchUserInfo(){
+    return this.http
+    .get(`https://animal-crossing-92e14.firebaseio.com/users/${this.id}/profile.json`)
+    .pipe(map(responseData => {
+      const loadedData =[];
+      for (const key in responseData) {
+        if (responseData.hasOwnProperty(key)){
+          loadedData.push({ ...responseData[key], newID: key})
+        }
+      }
+      return loadedData;
+    })
+    )
+  }
+}
