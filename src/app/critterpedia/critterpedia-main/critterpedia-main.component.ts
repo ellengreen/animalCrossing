71b2f448a9fff61f6ba82ce9d54c58887589a7e2
Ellen <eegreen.95@gmail.com>
2020-08-19 15:56:02 -0500

    hover over all links. simplifying variables

--- src/app/critterpedia/critterpedia-main/critterpedia-main.component.ts
+++ src/app/critterpedia/critterpedia-main/critterpedia-main.component.ts
@@ -12,186 +12,235 @@ import { AngularFireAuth } from '@angular/fire/auth';
 })
 export class CritterpediaMainComponent implements OnInit {
 
+  bugsList: any;
+  fishList: any;
+  seaList: any;
+  vendor: string;
+  critterList: any;
+  critterType: string;
+
   constructor(private ns: NookipediaService, private db: FirebaseService, private ds: CurrentDateService, private afAuth: AngularFireAuth){}
 
-  bugs: any;
-  fish: any;
-  sea: any;
-  critters: any;
-  critterType: any;
-  vendor: any;
-  loadedBugs: any;
-  loadedFish: any;
-  loadedSea: any;
-  bugView: boolean;
-  fishView: boolean;
-  seaView: boolean;
-  selectedCritter: any;
-  noUser: boolean;
-  
-    ngOnInit(){
-      this.ns.getBugs().subscribe(data=> {
-        this.bugs = data;
-        console.log(this.bugs)
-        this.critters = this.bugs;
-        this.critterType ='bugs';
-        this.vendor='Flick';
-        this.currentlyAvailable();
-      })
-      this.ns.getFish().subscribe(data => {
-        this.fish = data;
-      })
-      this.ns.getSea().subscribe(data => {
-        this.sea = data;
-      })
-      this.load();
-    }
-    load(){
-      this.db.fetchBugs().subscribe(bugs =>{
-        this.loadedBugs=bugs;
-      })
-      this.db.fetchFish().subscribe(fish =>{
-        this.loadedFish=fish;
-      })
-    this.afAuth.authState.subscribe(user => {
-      if (user){
-        this.noUser = false;
-      } else { this.noUser = true }})
-    }
-  
-    onFish(){
-      this.critters=this.fish;
-      this.bugView=false;
-      this.fishView=true;
-      this.seaView=false;
-      this.vendor='CJ';
-      this.critterType='fish';
-      this.currentlyAvailable();
-    }
-  
-    onBugs(){
-      this.critters=this.bugs;
-      this.fishView=false;
-      this.bugView=true;
-      this.seaView=false;
-      this.vendor='Flick';
-      this.critterType ='bugs';
-      this.currentlyAvailable();
-    }
-  
-    onSea(){
-      this.critters=this.sea;
-      this.seaView=true;
-      this.bugView=false;
-      this.fishView=false;
-      this.vendor='Pascal';
-      this.critterType ='sea';
-      this.currentlyAvailable();
-    }
+  // bugs: any;
+  // fish: any;
+  // sea: any;
+  // critters: any;
+  // critterType: any;
+  // vendor: any;
+  // loadedBugs: any;
+  // loadedFish: any;
+  // loadedSea: any;
+  // bugView: boolean;
+  // fishView: boolean;
+  // seaView: boolean;
+  // selectedCritter: any;
+  // noUser: boolean;
 
-    onSelect(c:any){
-      this.load();
-      this.selectedCritter = c;
-      this.dupe(this.selectedCritter);
-      if(this.duplicate.includes(this.selectedCritter['id'])){
-        this.aDupe=true 
-      } else { this.aDupe=false}
-    }
-  
-    showAll(){
-      if (this.fishView){
-        this.critters=this.fish;
-      } else if (this.bugView) {
-        this.critters=this.bugs;
-      } else {
-        this.critters=this.sea;
+  // fishValues: ['Price', "CJ's Price", 'Rarity', 'Location', 'Shadow Size', 'Avaiable Times', 'Northern Months', 'Southern Months'];
+  // bugValues: ['Price', "Flick's Price", 'Rarity', 'Location', 'Avaiable Times', 'Northern Months', 'Southern Months'];
+  // seaValues: ['Price', 'Shadow Size', 'Speed', 'Available Times', 'Northern Months', 'Southern Months'];
+
+     ngOnInit(){
+       
+       this.ns.getBugs().subscribe(data => {
+         this.bugsList = data;
+         this.getCritterInfo('bugs');
+       });
+
+       this.ns.getFish().subscribe(data => {
+         this.fishList = data;
+       })
+
+       this.ns.getSea().subscribe(data => {
+         this.seaList = data;
+       })
+
       }
-    }
-  
-    showMine(){
-      if (this.fishView){
-        this.db.fetchFish()
-        .subscribe(fish =>{
-        this.critters=fish;
-        })} else {
-          this.db.fetchBugs().subscribe(bugs=>{
-            this.critters=bugs;
-          })
-        }
-    }
-  
-    showCurrent(){
-      this.catchableCritters();
-      this.critters=this.thisMonth
-    }
-  
-    hourMethod(id){
-      return this.thisHour.some((item) => item.id == id);
-    }
-  
-    currentMonth = this.ds.currentMonth;
-    hour = this.ds.thisHour;
-    thisHour = [];
-    thisMonth = [];
-  
-    catchableCritters(){
-      if (this.fishView){
-        this.critters=this.fish;
-      } else if (this.bugView) {
-        this.critters=this.bugs;
-      } else {
-        this.critters=this.sea;
-      }
-      this.thisMonth=[];
-      Object.keys(this.critters).forEach(key=> {
-        if (this.critters[key]['availability']['month-array-northern'].includes(this.currentMonth)){
-          this.thisMonth.push(this.critters[key]);
-        }
-      })
-    }
-  
-    currentlyAvailable(){
-      this.thisHour=[];
-      Object.keys(this.critters).forEach(key=> {
-        if (this.critters[key]['availability']['time-array'].includes(this.hour) && (this.critters[key]['availability']['month-array-northern'].includes(this.currentMonth))){
-          this.thisHour.push(this.critters[key]);
-        }
-      })
-    }
-    
-    aDupe: boolean;
-    addFish(selectedCritter){
-      this.db.addFish(selectedCritter);
-      this.aDupe=true;
-    }
-    addBugs(selectedCritter){
-      this.db.addBug(selectedCritter);
-      this.aDupe=true;
-    }
-    // addSea(selectedCritter){
 
-    // }
-  
-    duplicate = [];
-    loaded: any;
-    dupe(selectedCritter: any){
-      if (this.fishView){this.loaded = this.loadedFish
-        } else {this.loaded = this.loadedBugs}
-      Object.keys(this.loaded).forEach(key=>{
-        if (this.loaded[key]['id']===(this.selectedCritter['id'])){
-          this.duplicate.push(this.selectedCritter['id']);
-        }
-      })
+  getCritterInfo(critter: string){
+    if (critter === 'bugs'){
+      this.vendor = 'Flick';
+      this.critterList = this.bugsList;
+      this.critterType = 'bugs';
     }
 
-    id: any;
-    delete(selectedCritter){
-      this.id = selectedCritter['newID'];
-      this.db.deleteCritter(this.id, this.critterType);
-      this.aDupe = false;
+    if (critter === 'fish'){
+      this.critterList = this.fishList;
+      this.critterType = 'fish';
+      this.vendor = 'CJ';
     }
+
+    if (critter === 'sea'){
+      this.critterType = 'sea';
+      this.critterList = this.seaList;
+      this.vendor = 'Pascal';
+    }
+  } 
+
+  onSelect(somethin){
+
   }
+  //     this.ns.getBugs().subscribe(data=> {
+  //       this.bugs = data;
+  //       console.log(this.bugs)
+  //       this.critters = this.bugs;
+  //       this.critterType ='bugs';
+  //       this.vendor='Flick';
+  //       this.currentlyAvailable();
+  //     })
+  //     this.ns.getFish().subscribe(data => {
+  //       this.fish = data;
+  //     })
+  //     this.ns.getSea().subscribe(data => {
+  //       this.sea = data;
+  //     })
+  //     this.load();
+  //   }
+  //   load(){
+  //     this.db.fetchBugs().subscribe(bugs =>{
+  //       this.loadedBugs=bugs;
+  //     })
+  //     this.db.fetchFish().subscribe(fish =>{
+  //       this.loadedFish=fish;
+  //     })
+  //   this.afAuth.authState.subscribe(user => {
+  //     if (user){
+  //       this.noUser = false;
+  //     } else { this.noUser = true }})
+     }
   
+  //   onFish(){
+  //     this.critters=this.fish;
+  //     this.bugView=false;
+  //     this.fishView=true;
+  //     this.seaView=false;
+  //     this.vendor='CJ';
+  //     this.critterType='fish';
+  //     this.currentlyAvailable();
+  //   }
+  
+  //   onBugs(){
+  //     this.critters=this.bugs;
+  //     this.fishView=false;
+  //     this.bugView=true;
+  //     this.seaView=false;
+  //     this.vendor='Flick';
+  //     this.critterType ='bugs';
+  //     this.currentlyAvailable();
+  //   }
+  
+  //   onSea(){
+  //     this.critters=this.sea;
+  //     this.seaView=true;
+  //     this.bugView=false;
+  //     this.fishView=false;
+  //     this.vendor='Pascal';
+  //     this.critterType ='sea';
+  //     this.currentlyAvailable();
+  //   }
+
+  //   onSelect(c:any){
+  //     this.load();
+  //     this.selectedCritter = c;
+  //     this.dupe(this.selectedCritter);
+  //     if(this.duplicate.includes(this.selectedCritter['id'])){
+  //       this.aDupe=true 
+  //     } else { this.aDupe=false}
+  //   }
+  
+  //   showAll(){
+  //     if (this.fishView){
+  //       this.critters=this.fish;
+  //     } else if (this.bugView) {
+  //       this.critters=this.bugs;
+  //     } else {
+  //       this.critters=this.sea;
+  //     }
+  //   }
+  
+  //   showMine(){
+  //     if (this.fishView){
+  //       this.db.fetchFish()
+  //       .subscribe(fish =>{
+  //       this.critters=fish;
+  //       })} else {
+  //         this.db.fetchBugs().subscribe(bugs=>{
+  //           this.critters=bugs;
+  //         })
+  //       }
+  //   }
+  
+  //   showCurrent(){
+  //     this.catchableCritters();
+  //     this.critters=this.thisMonth
+  //   }
+  
+  //   hourMethod(id){
+  //     return this.thisHour.some((item) => item.id == id);
+  //   }
+  
+  //   currentMonth = this.ds.currentMonth;
+  //   hour = this.ds.thisHour;
+  //   thisHour = [];
+  //   thisMonth = [];
+  
+  //   catchableCritters(){
+  //     if (this.fishView){
+  //       this.critters=this.fish;
+  //     } else if (this.bugView) {
+  //       this.critters=this.bugs;
+  //     } else {
+  //       this.critters=this.sea;
+  //     }
+  //     this.thisMonth=[];
+  //     Object.keys(this.critters).forEach(key=> {
+  //       if (this.critters[key]['availability']['month-array-northern'].includes(this.currentMonth)){
+  //         this.thisMonth.push(this.critters[key]);
+  //       }
+  //     })
+  //   }
+  
+  //   currentlyAvailable(){
+  //     this.thisHour=[];
+  //     Object.keys(this.critters).forEach(key=> {
+  //       if (this.critters[key]['availability']['time-array'].includes(this.hour) && (this.critters[key]['availability']['month-array-northern'].includes(this.currentMonth))){
+  //         this.thisHour.push(this.critters[key]);
+  //       }
+  //     })
+  //   }
+    
+  //   aDupe: boolean;
+  //   addFish(selectedCritter){
+  //     this.db.addFish(selectedCritter);
+  //     this.aDupe=true;
+  //   }
+  //   addBugs(selectedCritter){
+  //     this.db.addBug(selectedCritter);
+  //     this.aDupe=true;
+  //   }
+  //   // addSea(selectedCritter){
+
+  //   // }
+  
+  //   duplicate = [];
+  //   loaded: any;
+  //   dupe(selectedCritter: any){
+  //     if (this.fishView){this.loaded = this.loadedFish
+  //       } else {this.loaded = this.loadedBugs}
+  //     Object.keys(this.loaded).forEach(key=>{
+  //       if (this.loaded[key]['id']===(this.selectedCritter['id'])){
+  //         this.duplicate.push(this.selectedCritter['id']);
+  //       }
+  //     })
+  //   }
+
+  //   id: any;
+  //   delete(selectedCritter){
+  //     this.id = selectedCritter['newID'];
+  //     this.db.deleteCritter(this.id, this.critterType);
+  //     this.aDupe = false;
+  //   }
+  // }
   
   
   
